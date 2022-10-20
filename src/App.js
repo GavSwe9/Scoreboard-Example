ebf8b0945196346ea8984b3fd6f82a628b2d7572
Gavin Sweeney <gavswe19@gmail.com>
2022-10-20 18:50:29 -0400

    Create scoreboard games

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { GameCard } from './Components/GameCard';
+// import GameCard from './Components/GameCard'
 
 function App() {
+  let [loading, setLoading] = useState(true)
+  let [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetchData()
+    async function fetchData() {
+      let res = await fetch('https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=2022-07-01')
+      res = await res.json()
+
+      setGames(res.dates[0].games)
+      setLoading(false)
+    }
+  }, [])
+
+  const gameCards = games.map(g => (
+    <GameCard gameId={g.gamePk} />
+  ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {gameCards}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/GameCard.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+
+export function GameCard(props) {
+    let [loading, setLoading] = useState(true)
+    let [data, setData] = useState({})
+
+    useEffect(() => {
+        setLoading(true)
+        fetchData()
+
+        async function fetchData() {
+            let res = await fetch(`https://statsapi.mlb.com/api/v1.1/game/${props.gameId}/feed/live`);
+
+            res = await res.json();
+            setData(res);
+            setLoading(false)
+        }
+
+    }, [])
+
+    if (loading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <div className='GameContainer'>
+                    <div className='TeamContainer'>
+                        <div>
+                            <img className='TeamLogo' src={`https://www.mlbstatic.com/team-logos/team-cap-on-light/${data.gameData.teams.away.id}.svg`} />
+                        </div>
+                        <div>
+                            {data.gameData.teams.away.name}
+                        </div>
+                        <div>
+                            {data.liveData.boxscore.teams.away.teamStats.batting.runs}
+                        </div>
+                    </div>
+
+                    <div className='TeamContainer'>
+                        <div>
+                            <img className='TeamLogo' src={`https://www.mlbstatic.com/team-logos/team-cap-on-light/${data.gameData.teams.home.id}.svg`} />
+                        </div>
+                        <div>
+                            {data.gameData.teams.home.name}
+                        </div>
+                        <div>
+                            {data.liveData.boxscore.teams.home.teamStats.batting.runs}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
